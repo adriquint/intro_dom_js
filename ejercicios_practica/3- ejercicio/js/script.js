0baebe13f3caafd9ebc1939bbd68913239dfcc6b
adriquint <125844291+adriquint@users.noreply.github.com>
2023-05-30 18:32:13 -0300

    Add files via upload

--- ejercicios_practica/3- ejercicio/js/script.js
+++ ejercicios_practica/3- ejercicio/js/script.js
@@ -15,3 +15,19 @@ Cuando un botón es presionado, se debe agregar la clase
 "fuego" al botón (deberá cambiar de color)
 
 */
+
+const btn1 = document.getElementById("boton1") /*console.log(btn1)*/
+const btn2 = document.getElementById("boton2") /*console.log(btn2)*/
+const btn3 = document.getElementById("boton3") /*console.log(btn3)*/
+
+btn1.addEventListener("click", function() {
+    btn1.classList.add("fuego");
+})
+
+btn2.addEventListener("click", function() {
+    btn2.classList.add("fuego");
+})
+
+btn3.addEventListener("click", function() {
+    btn3.classList.add("fuego");
+})
