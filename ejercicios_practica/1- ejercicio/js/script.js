1933e72fa95f33cf93759b39f2e09994902751a3
adriquint <125844291+adriquint@users.noreply.github.com>
2023-05-30 18:30:18 -0300

    Add files via upload

--- ejercicios_practica/1- ejercicio/js/script.js
+++ ejercicios_practica/1- ejercicio/js/script.js
@@ -8,9 +8,15 @@
 /* Enunciado
 
 1)
-Leer el título del "header" del post e imprimirlo en consola.   
+Leer el título del "header" del post e imprimirlo en consola.
+
 
 2)
 Cambiar título utilizando javascript por otro cualquiera.
 
 */
+
+let titulo = document.querySelector("h2")
+console.log(titulo)
+
+titulo.textContent = "Accediendo desde Js:"
