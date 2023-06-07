e307bc06dce48037bdfad704727e6978032d7ac1
adriquint <125844291+adriquint@users.noreply.github.com>
2023-06-06 22:38:56 -0300

    Add files via upload

--- ejercicios_practica/2- ejercicio/js/script.js
+++ ejercicios_practica/2- ejercicio/js/script.js
@@ -18,10 +18,22 @@ luego a todos los botones agregar la clase "agua"
 
 */
 
-let boton1 = document.getElementById("boton1") /*console.log(boton1)*/
-let boton2 = document.getElementById("boton2") /*console.log(boton2)*/
-let boton3 = document.getElementById("boton3") /*console.log(boton3)*/
+let boton1 = document.getElementById("boton1") // console.log(boton1)
+let boton2 = document.getElementById("boton2") // console.log(boton2)
+let boton3 = document.getElementById("boton3") // console.log(boton3)
 
 boton1.textContent = "PEGAR"
 boton2.textContent = "COPIAR"
 boton3.textContent = "CORTAR"
+
+
+const botones = document.getElementsByClassName("btn")
+console.log(botones)
+
+botones[0].classList.add("agua")
+botones[1].classList.add("agua")
+botones[2].classList.add("agua")
+
+/*boton1.classList.add("agua")
+boton2.classList.add("agua")
+boton3.classList.add("agua")*/
